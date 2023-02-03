93ecaa0bae2892a423c4131c250d691ced7759b8
Zakariae El Hichem <zakariae.el.hichem@desjardins.com>
2023-02-02 22:54:34 -0500

    Add authentication providers

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # jwt-security
 A demo for Spring security authentication based on JWT
+This app call the https://github.com/LoZzR/opt-server as an OTP server
---
+++ src/main/java/com/jwt/security/providers/UsernamePasswordAuthenticationProvider.java
@@ -0,0 +1,29 @@
+package com.jwt.security.providers;
+
+import com.jwt.security.authentication.UsernamePasswordAuthentication;
+import com.jwt.security.proxy.AuthenticationServerProxy;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class UsernamePasswordAuthenticationProvider implements AuthenticationProvider {
+
+    private final AuthenticationServerProxy proxy;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        String username = authentication.getName();
+        String password = String.valueOf(authentication.getCredentials());
+        proxy.sendAuth(username, password);
+        return new UsernamePasswordAuthentication(username, password);
+    }
+
+    @Override
+    public boolean supports(Class<?> aClass) {
+        return UsernamePasswordAuthentication.class.isAssignableFrom(aClass);
+    }
+}
