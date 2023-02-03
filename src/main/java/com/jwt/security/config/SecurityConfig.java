5f8ffe5c8dafcb34241f496447d2efe0c1a12e54
Zakariae El Hichem <zakariae.el.hichem@desjardins.com>
2023-02-02 23:07:18 -0500

    Add security configuration

---
+++ src/main/java/com/jwt/security/config/SecurityConfig.java
@@ -0,0 +1,60 @@
+package com.jwt.security.config;
+
+import com.jwt.security.filters.InitialAuthenticationFilter;
+import com.jwt.security.filters.JwtAuthenticationFilter;
+import com.jwt.security.providers.OtpAuthenticationProvider;
+import com.jwt.security.providers.UsernamePasswordAuthenticationProvider;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+
+@Configuration
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final InitialAuthenticationFilter initialAuthenticationFilter;
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
+
+    private final OtpAuthenticationProvider otpAuthenticationProvider;
+
+    private final UsernamePasswordAuthenticationProvider usernamePasswordAuthenticationProvider;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) {
+        auth.authenticationProvider(otpAuthenticationProvider)
+            .authenticationProvider(usernamePasswordAuthenticationProvider);
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+
+        http.addFilterAt(
+                initialAuthenticationFilter,
+                BasicAuthenticationFilter.class)
+            .addFilterAfter(
+                jwtAuthenticationFilter,
+                BasicAuthenticationFilter.class
+            );
+
+        http.authorizeRequests()
+                .anyRequest().authenticated();
+    }
+
+    /*Adds the AuthenticationManager
+     to the Spring context so that we can
+     autowire it from the filter class*/
+    @Override
+    @Bean
+    protected AuthenticationManager authenticationManager() throws Exception {
+        return super.authenticationManager();
+    }
+}
