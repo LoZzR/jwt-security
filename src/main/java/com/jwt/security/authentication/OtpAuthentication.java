991fb2f6391d9b64b6bd0ade60cfe24c7f0a08fb
Zakariae El Hichem <zakariae.el.hichem@desjardins.com>
2023-02-02 22:43:52 -0500

    Add authentication objects

---
+++ src/main/java/com/jwt/security/authentication/OtpAuthentication.java
@@ -0,0 +1,10 @@
+package com.jwt.security.authentication;
+
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+
+public class OtpAuthentication extends UsernamePasswordAuthenticationToken {
+
+    public OtpAuthentication(Object principal, Object credentials) {
+        super(principal, credentials);
+    }
+}
