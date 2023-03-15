25b8cdc618e5258ddc154aabea717af1f0ad0378
Zakariae El Hichem <zakariae.el.hichem@desjardins.com>
2023-03-14 23:25:49 -0400

    Use the correct AuthenticationProvider

--- src/main/java/com/jwt/security/providers/UsernamePasswordAuthenticationProvider.java
+++ src/main/java/com/jwt/security/providers/UsernamePasswordAuthenticationProvider.java
@@ -4,6 +4,7 @@ import com.jwt.security.authentication.UsernamePasswordAuthentication;
 import com.jwt.security.proxy.AuthenticationServerProxy;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.stereotype.Component;
@@ -19,7 +20,7 @@ public class UsernamePasswordAuthenticationProvider implements AuthenticationPro
         String username = authentication.getName();
         String password = String.valueOf(authentication.getCredentials());
         proxy.sendAuth(username, password);
-        return new UsernamePasswordAuthentication(username, password);
+        return new UsernamePasswordAuthenticationToken(username, password);
     }
 
     @Override
